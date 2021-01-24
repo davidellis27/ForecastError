f73eb482e90a8d0c131fc6abf97c4d5f903ec6cb
davidellis27 <david.ellis.27@gmail.com>
2021-01-24 18:33:12 -0500

    updates

--- ForecastError.py
+++ ForecastError.py
@@ -447,6 +447,8 @@ for x in range(1, 11):
     for key, value in df.items():
         print(key, ' : ', value)
 
+    _df = {"Tests" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]}
+    df.update(_df)
     df_list.append(df)
 
     print('')
