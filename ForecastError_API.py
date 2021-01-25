b6c57ce7b989b3b54aa6740ed2aec5ee6277b23f
davidellis27 <david.ellis.27@gmail.com>
2021-01-25 00:46:54 -0500

    split out metrics functions

--- ForecastError_API.py
+++ ForecastError_API.py
@@ -1,293 +1,11 @@
 #!/usr/bin/env python3
 
-import numpy as np
 from numpy import random
 from flask import Flask
 import pandas as pd
 import plotly.express as px
-
-
-def _safe_div(a, b):
-    with np.errstate(divide='ignore', invalid='ignore'):
-        c = np.true_divide(a, b)
-        c[c == np.inf] = 0
-        c = np.nan_to_num(c)
-    return c
-
-
-def _error(actual: np.ndarray, predicted: np.ndarray):
-    """ Simple error """
-    #    return actual - predicted
-    return predicted - actual
-
-
-def _percentage_error(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Percentage error
-    Note: result is NOT multiplied by 100
-    """
-    return _safe_div(_error(actual, predicted), actual)
-
-
-def _naive_forecasting(actual: np.ndarray, seasonality: int = 1):
-    """ Naive forecasting method which just repeats previous samples """
-    return actual[:-seasonality]
-
-
-def _relative_error(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Relative Error """
-    if benchmark is None or isinstance(benchmark, int):
-        # If no benchmark prediction provided - use naive forecasting
-        if not isinstance(benchmark, int):
-            seasonality = 1
-        else:
-            seasonality = benchmark
-
-        #        return _error(actual[seasonality:], predicted[seasonality:]) /\
-        #               (_error(actual[seasonality:], _naive_forecasting(actual, seasonality)) + EPSILON)
-
-        return _safe_div(_error(actual[seasonality:], predicted[seasonality:]),
-                         _error(actual[seasonality:], _naive_forecasting(actual, seasonality)))
-
-    #    return _error(actual, predicted) / (_error(actual, benchmark) + EPSILON)
-    return _safe_div(_error(actual, predicted), _error(actual, benchmark))
-
-
-def _bounded_relative_error(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Bounded Relative Error """
-    if benchmark is None or isinstance(benchmark, int):
-        # If no benchmark prediction provided - use naive forecasting
-        if not isinstance(benchmark, int):
-            seasonality = 1
-        else:
-            seasonality = benchmark
-
-        abs_err = np.abs(_error(actual[seasonality:], predicted[seasonality:]))
-        abs_err_bench = np.abs(_error(actual[seasonality:], _naive_forecasting(actual, seasonality)))
-    else:
-        abs_err = np.abs(_error(actual, predicted))
-        abs_err_bench = np.abs(_error(actual, benchmark))
-
-    #    return abs_err / (abs_err + abs_err_bench + EPSILON)
-    return _safe_div(abs_err, (abs_err + abs_err_bench))
-
-
-def _geometric_mean(a, axis=0, dtype=None):
-    """ Geometric mean """
-    if not isinstance(a, np.ndarray):  # if not an ndarray object attempt to convert it
-        log_a = np.log(np.array(a, dtype=dtype))
-    elif dtype:  # Must change the default dtype allowing array type
-        if isinstance(a, np.ma.MaskedArray):
-            log_a = np.log(np.ma.asarray(a, dtype=dtype))
-        else:
-            log_a = np.log(np.asarray(a, dtype=dtype))
-    else:
-        log_a = np.log(a)
-    return np.exp(log_a.mean(axis=axis))
-
-
-def mse(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Squared Error """
-    return np.mean(np.square(_error(actual, predicted)))
-
-
-def rmse(actual: np.ndarray, predicted: np.ndarray):
-    """ Root Mean Squared Error """
-    return np.sqrt(mse(actual, predicted))
-
-
-def nrmse(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Root Mean Squared Error """
-    # return rmse(actual, predicted) / (actual.max() - actual.min())
-    return rmse(actual, predicted) / (np.max(actual) - np.min(actual))
-
-
-def me(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Error """
-    return np.mean(_error(actual, predicted))
-
-
-def mae(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Absolute Error """
-    return np.mean(np.abs(_error(actual, predicted)))
-
-
-mad = mae  # Mean Absolute Deviation (it is the same as MAE)
-
-
-def gmae(actual: np.ndarray, predicted: np.ndarray):
-    """ Geometric Mean Absolute Error """
-    return _geometric_mean(np.abs(_error(actual, predicted)))
-
-
-def mdae(actual: np.ndarray, predicted: np.ndarray):
-    """ Median Absolute Error """
-    return np.median(np.abs(_error(actual, predicted)))
-
-
-def mpe(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Percentage Error """
-    return np.mean(_percentage_error(actual, predicted))
-
-
-def mape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Absolute Percentage Error
-    Properties:
-        + Easy to interpret
-        + Scale independent
-        - Biased, not symmetric
-        - Undefined when actual[t] == 0
-    Note: result is NOT multiplied by 100
-    """
-    return np.mean(np.abs(_percentage_error(actual, predicted)))
-
-
-def mdape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Median Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.median(np.abs(_percentage_error(actual, predicted)))
-
-
-def smape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Symmetric Mean Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.mean(2.0 * np.abs(actual - predicted) / ((np.abs(actual) + np.abs(predicted)) + EPSILON))
-    return np.mean(2.0 * (_safe_div(np.abs(actual - predicted), (np.abs(actual) + np.abs(predicted)))))
-
-
-def smdape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Symmetric Median Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.median(2.0 * np.abs(actual - predicted) / ((np.abs(actual) + np.abs(predicted)) + EPSILON))
-    return np.median(2.0 * (_safe_div(np.abs(actual - predicted), (np.abs(actual) + np.abs(predicted)))))
-
-
-def bias_tracking(actual: np.ndarray, predicted: np.ndarray):
-    return np.mean(_safe_div((actual - predicted), np.abs(actual - predicted)))
-
-
-def bias_nfm(actual: np.ndarray, predicted: np.ndarray):
-    return np.mean(_safe_div((actual - predicted), (actual + predicted)))
-
-
-def maape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Arctangent Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.mean(np.arctan(np.abs((actual - predicted) / (actual + EPSILON))))
-    return np.mean(np.arctan(np.abs(_safe_div((actual - predicted), actual))))
-
-
-def mase(actual: np.ndarray, predicted: np.ndarray, seasonality: int = 1):
-    """
-    Mean Absolute Scaled Error
-    Baseline (benchmark) is computed with naive forecasting (shifted by @seasonality)
-    """
-    return mae(actual, predicted) / mae(actual[seasonality:], _naive_forecasting(actual, seasonality))
-
-
-def std_ae(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Absolute Error """
-    __mae = mae(actual, predicted)
-    return np.sqrt(np.sum(np.square(_error(actual, predicted) - __mae)) / (len(actual) - 1))
-
-
-def std_ape(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Absolute Percentage Error """
-    __mape = mape(actual, predicted)
-    return np.sqrt(np.sum(np.square(_percentage_error(actual, predicted) - __mape)) / (len(actual) - 1))
-
-
-def rmspe(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Root Mean Squared Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.sqrt(np.mean(np.square(_percentage_error(actual, predicted))))
-
-
-def rmdspe(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Root Median Squared Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.sqrt(np.median(np.square(_percentage_error(actual, predicted))))
-
-
-def rmsse(actual: np.ndarray, predicted: np.ndarray, seasonality: int = 1):
-    """ Root Mean Squared Scaled Error """
-    q = np.abs(_error(actual, predicted)) / mae(actual[seasonality:], _naive_forecasting(actual, seasonality))
-    return np.sqrt(np.mean(np.square(q)))
-
-
-def inrse(actual: np.ndarray, predicted: np.ndarray):
-    """ Integral Normalized Root Squared Error """
-    return np.sqrt(np.sum(np.square(_error(actual, predicted))) / np.sum(np.square(actual - np.mean(actual))))
-
-
-def rrse(actual: np.ndarray, predicted: np.ndarray):
-    """ Root Relative Squared Error """
-    return np.sqrt(np.sum(np.square(actual - predicted)) / np.sum(np.square(actual - np.mean(actual))))
-
-
-def mre(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Relative Error """
-    return np.mean(_relative_error(actual, predicted, benchmark))
-
-
-def rae(actual: np.ndarray, predicted: np.ndarray):
-    """ Relative Absolute Error (aka Approximation Error) """
-    #    return np.sum(np.abs(actual - predicted)) / (np.sum(np.abs(actual - np.mean(actual))) + EPSILON)
-    return _safe_div(np.sum(np.abs(actual - predicted)), np.sum(np.abs(actual - np.mean(actual))))
-
-
-def mrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Relative Absolute Error """
-    return np.mean(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def mdrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Median Relative Absolute Error """
-    return np.median(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def gmrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Geometric Mean Relative Absolute Error """
-    return _geometric_mean(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def mbrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Bounded Relative Absolute Error """
-    return np.mean(_bounded_relative_error(actual, predicted, benchmark))
-
-
-def umbrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Unscaled Mean Bounded Relative Absolute Error """
-    __mbrae = mbrae(actual, predicted, benchmark)
-    return __mbrae / (1 - __mbrae)
-
-
-def mda(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Directional Accuracy """
-    # return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - predicted[:-1])).astype(int))
-    return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - predicted[:-1])))
-
-
-def wmape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Weighted Mean Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    se_actual_prod_mape = actual * _safe_div(abs(actual - predicted), actual)
-    return se_actual_prod_mape.sum() / actual.sum()
+from ForecastError_Metrics import *
+import datetime
 
 
 app = Flask(__name__)
@@ -330,8 +48,6 @@ def forecasterror(iters=1):
         'WMAPE': wmape,
         'BIAS_TRACK': bias_tracking,
         'BIAS_NFM': bias_nfm,
-        'error': _error,
-        '%_error': _percentage_error,
     }
 
     METRICS_NAME = {
@@ -368,8 +84,6 @@ def forecasterror(iters=1):
         'WMAPE': 'Weighted Mean Absolute Percentage Error',
         'BIAS_TRACK': 'Bias Tracking Signal',
         'BIAS_NFM': 'Bias Normalized Forecast Metric',
-        'error': 'Simple Error',
-        '%_error': 'Percentage Error',
     }
 
     test_values_set = [
@@ -399,8 +113,6 @@ def forecasterror(iters=1):
     test_metrics = ['MAPE', 'MASE', 'MDAPE', 'WMAPE', 'SMAPE', 'ME', 'MAE', 'MPE', 'RMSE', 'NRMSE',
                     'BIAS_TRACK', 'BIAS_NFM', 'NRMSE']
 
-    num_iterations = int(iters)
-
     def evaluate(actual: np.ndarray, predicted: np.ndarray, metrics=('MAPE', 'MASE')):
         results = {}
         for name in metrics:
@@ -412,18 +124,22 @@ def forecasterror(iters=1):
         return results
 
     df_list = []
-
-    num_tests = len(test_values_set)
-
     output = ""
     output_array = []
+
+    num_tests = len(test_values_set)
+    num_iterations = int(iters)
+
+    now = datetime.datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M:%S"))
+    print('')
+    output += '{}<br><br>'.format(now.strftime("%Y-%m-%d %H:%M:%S"))
 
     for x in range(1, num_iterations + 1):
         print('START: {}'.format(x))
         output += 'START {}<br>'.format(x)
 
         tests = 1
-        # df = {}
         df = {"Test": []}
 
         for test_values in test_values_set:
@@ -465,7 +181,6 @@ def forecasterror(iters=1):
                 output_array.append(
                     [answer, '@@div style="text-align:right"@@@{:.2f}@@/div@@@'.format(round(answers[answer] * 100, 2)),
                      METRICS_NAME[answer]])
-                # output_array[0][1] = str.replace('&lt', '<')
 
                 if tests == 1:
                     _df = {answer: [round(answers[answer] * 100, 2)]}
--- ForecastError_CLI.py
+++ ForecastError_CLI.py
@@ -1,290 +1,8 @@
 #!/usr/bin/env python3
 
-import numpy as np
 from numpy import random
-
-
-def _safe_div(a, b):
-    with np.errstate(divide='ignore', invalid='ignore'):
-        c = np.true_divide(a, b)
-        c[c == np.inf] = 0
-        c = np.nan_to_num(c)
-    return c
-
-
-def _error(actual: np.ndarray, predicted: np.ndarray):
-    """ Simple error """
-    #    return actual - predicted
-    return predicted - actual
-
-
-def _percentage_error(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Percentage error
-    Note: result is NOT multiplied by 100
-    """
-    return _safe_div(_error(actual, predicted), actual)
-
-
-def _naive_forecasting(actual: np.ndarray, seasonality: int = 1):
-    """ Naive forecasting method which just repeats previous samples """
-    return actual[:-seasonality]
-
-
-def _relative_error(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Relative Error """
-    if benchmark is None or isinstance(benchmark, int):
-        # If no benchmark prediction provided - use naive forecasting
-        if not isinstance(benchmark, int):
-            seasonality = 1
-        else:
-            seasonality = benchmark
-
-        #        return _error(actual[seasonality:], predicted[seasonality:]) /\
-        #               (_error(actual[seasonality:], _naive_forecasting(actual, seasonality)) + EPSILON)
-
-        return _safe_div(_error(actual[seasonality:], predicted[seasonality:]),
-                         _error(actual[seasonality:], _naive_forecasting(actual, seasonality)))
-
-    #    return _error(actual, predicted) / (_error(actual, benchmark) + EPSILON)
-    return _safe_div(_error(actual, predicted), _error(actual, benchmark))
-
-
-def _bounded_relative_error(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Bounded Relative Error """
-    if benchmark is None or isinstance(benchmark, int):
-        # If no benchmark prediction provided - use naive forecasting
-        if not isinstance(benchmark, int):
-            seasonality = 1
-        else:
-            seasonality = benchmark
-
-        abs_err = np.abs(_error(actual[seasonality:], predicted[seasonality:]))
-        abs_err_bench = np.abs(_error(actual[seasonality:], _naive_forecasting(actual, seasonality)))
-    else:
-        abs_err = np.abs(_error(actual, predicted))
-        abs_err_bench = np.abs(_error(actual, benchmark))
-
-    #    return abs_err / (abs_err + abs_err_bench + EPSILON)
-    return _safe_div(abs_err, (abs_err + abs_err_bench))
-
-
-def _geometric_mean(a, axis=0, dtype=None):
-    """ Geometric mean """
-    if not isinstance(a, np.ndarray):  # if not an ndarray object attempt to convert it
-        log_a = np.log(np.array(a, dtype=dtype))
-    elif dtype:  # Must change the default dtype allowing array type
-        if isinstance(a, np.ma.MaskedArray):
-            log_a = np.log(np.ma.asarray(a, dtype=dtype))
-        else:
-            log_a = np.log(np.asarray(a, dtype=dtype))
-    else:
-        log_a = np.log(a)
-    return np.exp(log_a.mean(axis=axis))
-
-
-def mse(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Squared Error """
-    return np.mean(np.square(_error(actual, predicted)))
-
-
-def rmse(actual: np.ndarray, predicted: np.ndarray):
-    """ Root Mean Squared Error """
-    return np.sqrt(mse(actual, predicted))
-
-
-def nrmse(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Root Mean Squared Error """
-    # return rmse(actual, predicted) / (actual.max() - actual.min())
-    return rmse(actual, predicted) / (np.max(actual) - np.min(actual))
-
-
-def me(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Error """
-    return np.mean(_error(actual, predicted))
-
-
-def mae(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Absolute Error """
-    return np.mean(np.abs(_error(actual, predicted)))
-
-
-mad = mae  # Mean Absolute Deviation (it is the same as MAE)
-
-
-def gmae(actual: np.ndarray, predicted: np.ndarray):
-    """ Geometric Mean Absolute Error """
-    return _geometric_mean(np.abs(_error(actual, predicted)))
-
-
-def mdae(actual: np.ndarray, predicted: np.ndarray):
-    """ Median Absolute Error """
-    return np.median(np.abs(_error(actual, predicted)))
-
-
-def mpe(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Percentage Error """
-    return np.mean(_percentage_error(actual, predicted))
-
-
-def mape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Absolute Percentage Error
-    Properties:
-        + Easy to interpret
-        + Scale independent
-        - Biased, not symmetric
-        - Undefined when actual[t] == 0
-    Note: result is NOT multiplied by 100
-    """
-    return np.mean(np.abs(_percentage_error(actual, predicted)))
-
-
-def mdape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Median Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.median(np.abs(_percentage_error(actual, predicted)))
-
-
-def smape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Symmetric Mean Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.mean(2.0 * np.abs(actual - predicted) / ((np.abs(actual) + np.abs(predicted)) + EPSILON))
-    return np.mean(2.0 * (_safe_div(np.abs(actual - predicted), (np.abs(actual) + np.abs(predicted)))))
-
-
-def smdape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Symmetric Median Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.median(2.0 * np.abs(actual - predicted) / ((np.abs(actual) + np.abs(predicted)) + EPSILON))
-    return np.median(2.0 * (_safe_div(np.abs(actual - predicted), (np.abs(actual) + np.abs(predicted)))))
-
-
-def bias_tracking(actual: np.ndarray, predicted: np.ndarray):
-    return np.mean(_safe_div((actual - predicted), np.abs(actual - predicted)))
-
-
-def bias_nfm(actual: np.ndarray, predicted: np.ndarray):
-    return np.mean(_safe_div((actual - predicted), (actual + predicted)))
-
-
-def maape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Mean Arctangent Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    #    return np.mean(np.arctan(np.abs((actual - predicted) / (actual + EPSILON))))
-    return np.mean(np.arctan(np.abs(_safe_div((actual - predicted), actual))))
-
-
-def mase(actual: np.ndarray, predicted: np.ndarray, seasonality: int = 1):
-    """
-    Mean Absolute Scaled Error
-    Baseline (benchmark) is computed with naive forecasting (shifted by @seasonality)
-    """
-    return mae(actual, predicted) / mae(actual[seasonality:], _naive_forecasting(actual, seasonality))
-
-
-def std_ae(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Absolute Error """
-    __mae = mae(actual, predicted)
-    return np.sqrt(np.sum(np.square(_error(actual, predicted) - __mae)) / (len(actual) - 1))
-
-
-def std_ape(actual: np.ndarray, predicted: np.ndarray):
-    """ Normalized Absolute Percentage Error """
-    __mape = mape(actual, predicted)
-    return np.sqrt(np.sum(np.square(_percentage_error(actual, predicted) - __mape)) / (len(actual) - 1))
-
-
-def rmspe(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Root Mean Squared Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.sqrt(np.mean(np.square(_percentage_error(actual, predicted))))
-
-
-def rmdspe(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Root Median Squared Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    return np.sqrt(np.median(np.square(_percentage_error(actual, predicted))))
-
-
-def rmsse(actual: np.ndarray, predicted: np.ndarray, seasonality: int = 1):
-    """ Root Mean Squared Scaled Error """
-    q = np.abs(_error(actual, predicted)) / mae(actual[seasonality:], _naive_forecasting(actual, seasonality))
-    return np.sqrt(np.mean(np.square(q)))
-
-
-def inrse(actual: np.ndarray, predicted: np.ndarray):
-    """ Integral Normalized Root Squared Error """
-    return np.sqrt(np.sum(np.square(_error(actual, predicted))) / np.sum(np.square(actual - np.mean(actual))))
-
-
-def rrse(actual: np.ndarray, predicted: np.ndarray):
-    """ Root Relative Squared Error """
-    return np.sqrt(np.sum(np.square(actual - predicted)) / np.sum(np.square(actual - np.mean(actual))))
-
-
-def mre(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Relative Error """
-    return np.mean(_relative_error(actual, predicted, benchmark))
-
-
-def rae(actual: np.ndarray, predicted: np.ndarray):
-    """ Relative Absolute Error (aka Approximation Error) """
-    #    return np.sum(np.abs(actual - predicted)) / (np.sum(np.abs(actual - np.mean(actual))) + EPSILON)
-    return _safe_div(np.sum(np.abs(actual - predicted)), np.sum(np.abs(actual - np.mean(actual))))
-
-
-def mrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Relative Absolute Error """
-    return np.mean(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def mdrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Median Relative Absolute Error """
-    return np.median(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def gmrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Geometric Mean Relative Absolute Error """
-    return _geometric_mean(np.abs(_relative_error(actual, predicted, benchmark)))
-
-
-def mbrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Mean Bounded Relative Absolute Error """
-    return np.mean(_bounded_relative_error(actual, predicted, benchmark))
-
-
-def umbrae(actual: np.ndarray, predicted: np.ndarray, benchmark: np.ndarray = None):
-    """ Unscaled Mean Bounded Relative Absolute Error """
-    __mbrae = mbrae(actual, predicted, benchmark)
-    return __mbrae / (1 - __mbrae)
-
-
-def mda(actual: np.ndarray, predicted: np.ndarray):
-    """ Mean Directional Accuracy """
-    # return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - predicted[:-1])).astype(int))
-    return np.mean((np.sign(actual[1:] - actual[:-1]) == np.sign(predicted[1:] - predicted[:-1])))
-
-
-def wmape(actual: np.ndarray, predicted: np.ndarray):
-    """
-    Weighted Mean Absolute Percentage Error
-    Note: result is NOT multiplied by 100
-    """
-    se_actual_prod_mape = actual * _safe_div(abs(actual - predicted), actual)
-    return se_actual_prod_mape.sum() / actual.sum()
+from ForecastError_Metrics import *
+import datetime
 
 
 METRICS = {
@@ -321,8 +39,6 @@ METRICS = {
     'WMAPE': wmape,
     'BIAS_TRACK': bias_tracking,
     'BIAS_NFM': bias_nfm,
-    'error': _error,
-    '%_error': _percentage_error,
 }
 
 METRICS_NAME = {
@@ -359,20 +75,7 @@ METRICS_NAME = {
     'WMAPE': 'Weighted Mean Absolute Percentage Error',
     'BIAS_TRACK': 'Bias Tracking Signal',
     'BIAS_NFM': 'Bias Normalized Forecast Metric',
-    'error': 'Simple Error',
-    '%_error': 'Percentage Error',
 }
-
-
-def evaluate(actual: np.ndarray, predicted: np.ndarray, metrics=('MAPE', 'MASE')):
-    results = {}
-    for name in metrics:
-        try:
-            results[name] = METRICS[name](actual, predicted)
-        except Exception as err:
-            results[name] = np.nan
-            print('Unable to compute metric {0}: {1}'.format(name, err))
-    return results
 
 
 test_values_set = [
@@ -402,14 +105,32 @@ test_values_set = [
 test_metrics = ['MAPE', 'MASE', 'MDAPE', 'WMAPE', 'SMAPE', 'ME', 'MAE', 'MPE', 'RMSE', 'NRMSE',
                 'BIAS_TRACK', 'BIAS_NFM', 'NRMSE']
 
+
+def evaluate(actual: np.ndarray, predicted: np.ndarray, metrics=('MAPE', 'MASE')):
+    results = {}
+    for name in metrics:
+        try:
+            results[name] = METRICS[name](actual, predicted)
+        except Exception as err:
+            results[name] = np.nan
+            print('Unable to compute metric {0}: {1}'.format(name, err))
+    return results
+
+
 df_list = []
 
 num_tests = len(test_values_set)
+num_iterations = 4
 
-for x in range(1, 11):
+now = datetime.datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print('')
+
+for x in range(1, num_iterations + 1):
     print('START: {}'.format(x))
+
     tests = 1
-    df = {}
+    df = {"Test": []}
 
     for test_values in test_values_set:
         print('Test: {} / {}'.format(tests, num_tests))
@@ -441,15 +162,15 @@ for x in range(1, 11):
             else:
                 df[answer].append(round(answers[answer] * 100, 2))
 
+        df["Test"].append(tests)
+
         tests += 1
         print('')
 
+    df_list.append(df)
+
     for key, value in df.items():
         print(key, ' : ', value)
-
-    _df = {"Tests" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]}
-    df.update(_df)
-    df_list.append(df)
 
     print('')
     print('FINISH: {}'.format(x))
@@ -458,7 +179,9 @@ for x in range(1, 11):
 w = 1
 for thing in df_list:
     print(w)
+
     for key, value in thing.items():
         print(key, ' : ', value)
     print('')
+
     w += 1
