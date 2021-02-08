8b1734cd6f51a76252e2db1ca924ca09626a9591
davidellis27 <david.ellis.27@gmail.com>
2021-02-07 22:10:30 -0500

    building out Dash output

--- ForecastError_API _Dash.py
+++ ForecastError_API _Dash.py
@@ -65,37 +65,30 @@ def forecasterror(iters=1):
         return results
 
     df_list = []
-    output = ""
     output_array = []
-    output_list = []
+    metrics_array = []
 
     num_tests = len(test_values_set)
     num_iterations = int(iters)
-
-    a = html.Div(children='hello 1')
-    b = html.Div(children='hello 2')
+    num_iterations = 1
 
     for x in range(1, num_iterations + 1):
         the_text = 'START {}'.format(x)
-        output_start = html.P(children=the_text)
+        output_array.append(html.P(children=the_text))
 
         tests = 1
         df = {"Test": []}
 
         for test_values in test_values_set:
             the_text = 'Test: {} / {}'.format(tests, num_tests)
-            output_test_number = html.Div(children=the_text)
-            output_list.append(output_test_number)
+            output_array.append(html.Div(children=the_text))
 
-            output_array.append(["Forcast Range", test_values[0][0], test_values[0][1], test_values[0][2]])
+            params_array = [["Forcast Range", test_values[0][0], test_values[0][1], test_values[0][2]],
+                          ["VAR to Forecast Range", test_values[1][0], test_values[1][1], test_values[1][2]]]
+            _df = pd.DataFrame(params_array, columns=['Data Set', 'Low', 'High', 'Size'])
 
-            output_array.append(["VAR to Forecast Range", test_values[1][0], test_values[1][1], test_values[1][2]])
-
-            _df = pd.DataFrame(output_array, columns=['Data Set', 'Low', 'High', 'Size'])
-            output_array = []
-
-            table_id = 'table-{}'.format(tests)
-            output_list.append(html.P(dash_table.DataTable(
+            table_id = 'table-params-{}'.format(tests)
+            output_array.append(html.Div(dash_table.DataTable(
                 id=table_id,
                 columns=[{"name": i, "id": i} for i in _df.columns],
                 data=_df.to_dict('records'),
@@ -112,10 +105,6 @@ def forecasterror(iters=1):
                 }
             ), style={'width': '20%', 'display': 'inline-block'}))
 
-            output += _df.to_html(index=False, justify='left').replace('class="dataframe"',
-                                                                       'style="border-collapse:collapse" '
-                                                                       'class="dataframe"')
-
             the_forecast = np.array(
                 random.randint(low=test_values[0][0], high=test_values[0][1], size=test_values[0][2]))
 
@@ -130,40 +119,55 @@ def forecasterror(iters=1):
             # the_actuals = np.array([27.58,25.95,26.08,26.36,27.99,29.61,28.85,29.43,29.67,30.19,31.79,31.98])
             # the_forecast = np.array([27.58,26.765,26.015,26.220,27.175,28.8,29.23,29.14,29.55,29.93,30.99,31.885])
 
-            # print('Zeros: {}'.format(test_values[0][2] - np.count_nonzero(the_actuals)))
-            output += 'Zeros: {}<br>'.format(test_values[0][2] - np.count_nonzero(the_actuals))
-            # print('')
-            output += '<br>'
+            the_text = 'Zeros: {}'.format(test_values[0][2] - np.count_nonzero(the_actuals))
+            output_array.append(html.Div(children=the_text))
 
             answers = evaluate(the_actuals, the_forecast, metrics=test_metrics)
 
             for answer in answers:
-                # print(f'{answer:<11}{answers[answer]:>10.2f}  {METRICS_NAME[answer]:<}')
-                output_array.append(
-                    [answer, '@@div style="text-align:right"@@@{:.2f}@@/div@@@'.format(round(answers[answer], 2)),
-                     METRICS_NAME[answer]])
+                metrics_array.append([answer, '{:.2f}'.format(round(answers[answer], 2)), METRICS_NAME[answer]])
 
-                if tests == 1:
-                    _df = {answer: [round(answers[answer], 2)]}
-                    df.update(_df)
-                    _df.clear()
-                else:
-                    df[answer].append(round(answers[answer], 2))
+            # df["Test"].append(tests)
 
-            df["Test"].append(tests)
+            _df = pd.DataFrame(metrics_array, columns=['Metric', 'Value', 'Description'])
+            metrics_array = []
+
+            table_id = 'table-metrics-{}-{}'.format(x, tests)
+            # print(table_id)
+
+            output_array.append(html.Div(dash_table.DataTable(
+                id=table_id,
+                columns=[{"name": i, "id": i} for i in _df.columns],
+                data=_df.to_dict('records'),
+
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)'
+                    },
+                    {
+                        'if': {'column_id': 'Description'},
+                        'textAlign': 'left'
+                    }
+                ],
+                style_header_conditional=[
+                    {
+                        'if': {'column_id': 'Description'},
+                        'textAlign': 'left'
+                    }
+                ],
+                style_header={
+                    'backgroundColor': 'rgb(230, 230, 230)',
+                    'fontWeight': 'bold'
+                }
+            ), style={'width': '20%', 'display': 'inline-block'}))
 
             tests += 1
 
-            _df = pd.DataFrame(output_array, columns=['Metric', 'Value', 'Description'])
-            output_array = []
+        the_text = 'FINISH {}'.format(x)
+        output_array.append(html.Div(children=the_text))
 
-            output += _df.to_html(index=False, justify='left').replace('class="dataframe"',
-                                                                       'style="border-collapse:collapse" '
-                                                                       'class="dataframe"')
-
-            # print('')
-            output += '<br>'
-
+        """
         df_list.append(df)
 
         # for key, value in df.items():
@@ -172,14 +176,7 @@ def forecasterror(iters=1):
         _df = pd.DataFrame.from_dict(df)
         output += _df.to_html(index=False, justify='left')
 
-        # print('')
-        # print('FINISH: {}'.format(x))
-        # print('')
-        output += '<br>'
-        output += 'FINISH: {}<br>'.format(x)
-        output += '<br>'
 
-        """
         plot_data = pd.DataFrame.from_dict(df)
         plot = px.line(plot_data, x='Test', y=['MAPE', 'MDAPE', 'WAPE', 'SMAPE', 'MPE'],
                        title="Forecast Measurement Metrics - Percent Error - Run {}".format(x))
@@ -217,14 +214,15 @@ def forecasterror(iters=1):
 
     """
 
-    david_list = []
-    david_list.append(output_start)
-    david_list.extend(output_list)
-
+    # david_list = []
+    # david_list.append(output_start)
+    # david_list.extend(output_params)
+    # david_list.extend(output_zeros)
+    # david_list.extend(output_metrics)
 
     # return html.Div(children=[output_start, output_test_number])
-    return html.Div(children=david_list)
-
+    # return html.Div(children=david_list)
+    return html.Div(children=output_array)
 
 output_date = current_date()
 
